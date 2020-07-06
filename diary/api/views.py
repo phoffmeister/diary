1185d9bd6ba855e78b8bad9493dc5baf072ee4b4
Pierre <pierre.git@posteo.de>
2020-07-06 12:46:25 +0200

    photo: adds views to access photos
    In order to retrieve the photos themselves there is a special view
    that forwards the request so nginx can handle it. The view is protected
    by sessionauthentication that is set during login.

--- diary/api/views.py
+++ diary/api/views.py
@@ -1,20 +1,42 @@
-from rest_framework import generics, permissions, viewsets, mixins
-from rest_framework.response import Response
+from .models import PhotoEntry
+from .serializers import UserSerializer, EntryCollectionSerializer, TextEntrySerializer, DrinkEntrySerializer, DaySerializer, MedicationEntrySerializer, PhotoEntrySerializer
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_list_or_404, get_object_or_404
+from knox.auth import TokenAuthentication
 from knox.models import AuthToken
-from .serializers import LoginSerializer, UserSerializer, EntryCollectionSerializer, TextEntrySerializer, DrinkEntrySerializer, DaySerializer, MedicationEntrySerializer
-from django.shortcuts import get_list_or_404
+from rest_framework import generics, permissions, viewsets, mixins, status
+from rest_framework.decorators import authentication_classes, permission_classes, api_view
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from knox.views import LoginView as KnoxLoginView
+from django.contrib.auth import login
 
-class LoginAPI(generics.GenericAPIView):
-    serializer_class = LoginSerializer
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated])
+def protected_media(request, path):
+    file_name = request.path[request.path.rfind('/'):]
+    photo = get_object_or_404(PhotoEntry, owner=request.user, photo__endswith=file_name)
+    response = HttpResponse(status=200)
+    response['Content-Type'] = ''
+    response['X-Accel-Redirect'] = '/protected/' + path
+    return response
 
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
+
+class LoginAPI(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data
-        _, token = AuthToken.objects.create(user)
-        return Response({
-            "user": UserSerializer(user, context=self.get_serializer_context()).data,
-            "token": token            })
+        user = serializer.validated_data['user']
+        login(request, user)
+        AuthToken.objects.all().filter(user=user).delete()
+        return super(LoginAPI, self).post(request, format=None)
+
 
 class UserAPI(generics.RetrieveAPIView):
     permission_classes = [
@@ -44,7 +66,7 @@ class EntryCollectionViewSet(
     permission_classes = [
         permissions.IsAuthenticated,
     ]
-    
+
     serializer_class = EntryCollectionSerializer
 
     def get_queryset(self):
@@ -61,7 +83,7 @@ class TextEntryViewSet(
     permission_classes = [
         permissions.IsAuthenticated,
     ]
-    
+
     serializer_class = TextEntrySerializer
 
     def get_queryset(self):
@@ -82,7 +104,7 @@ class DrinkEntryViewSet(
     permission_classes = [
         permissions.IsAuthenticated,
     ]
-    
+
     serializer_class = DrinkEntrySerializer
 
     def get_queryset(self):
@@ -95,6 +117,28 @@ class DrinkEntryViewSet(
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+class PhotoEntryViewSet(
+        mixins.CreateModelMixin,
+        mixins.ListModelMixin,
+        mixins.RetrieveModelMixin,
+        viewsets.GenericViewSet):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+
+    serializer_class = PhotoEntrySerializer
+
+    def get_queryset(self):
+        queryset = self.request.user.photos.all()
+        collection = self.request.query_params.get('collection', None)
+        if collection:
+            return get_list_or_404(queryset, collection__id=collection)
+        return queryset
+
+    def perform_create(self, serializer):
+        print('i am called')
+        serializer.save(owner=self.request.user)
+
 class MedicationEntryViewSet(
         mixins.CreateModelMixin,
         mixins.ListModelMixin,
@@ -103,7 +147,7 @@ class MedicationEntryViewSet(
     permission_classes = [
         permissions.IsAuthenticated,
     ]
-    
+
     serializer_class = MedicationEntrySerializer
 
     def get_queryset(self):
