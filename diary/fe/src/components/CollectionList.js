46f9ed5af56159528edd5dce01a18ec2c02f29b4
Pierre <pierre.git@posteo.de>
2020-07-09 21:44:03 +0200

    EntryList: changes the layout and uses bootstrap components

--- diary/fe/src/components/CollectionList.js
+++ diary/fe/src/components/CollectionList.js
@@ -1,31 +1,51 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { matchPath, Link } from "react-router-dom";
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Table from 'react-bootstrap/Table';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faComment, faCapsules, faCocktail, faCamera } from '@fortawesome/free-solid-svg-icons'
 
 
-import { selectDate, getCollections, createCollection } from '../actions/collections'
+import { getCollections, createCollection } from '../actions/collections'
 
-class CollectionListItem extends Component {
+class CollectionListRow extends Component {
   render() {
-    let attributes = <Fragment>
-      { this.props.data.hasTextEntries ? <FontAwesomeIcon key="hasTextEntries" icon={faComment} /> : null}
-      { this.props.data.hasDrinkEntries ? <FontAwesomeIcon key="hasDrinkEntries" icon={faCocktail} /> : null}
-      { this.props.data.hasMedicationEntries ? <FontAwesomeIcon key="hasMedicationEntries" icon={faCapsules} /> : null}
-      { this.props.data.hasPhotoEntries ? <FontAwesomeIcon key="hasPhotoEntries" icon={faCamera} /> : null}
-    </Fragment>
+    let centerStyle = { textAlign: 'center' };
     return (
-      <li><Link to={`/day/${this.props.data.id}/`}>{this.props.data.date}</Link> {attributes}</li>
+      <tr>
+        <td>
+          <Link to={`/day/${this.props.data.id}/`}>{this.props.data.date}</Link>
+        </td>
+        <td style={centerStyle}>
+          {this.props.data.hasTextEntries ? <FontAwesomeIcon key="hasTextEntries" icon={faComment} /> : null}
+        </td>
+        <td style={centerStyle}>
+          {this.props.data.hasDrinkEntries ? <FontAwesomeIcon key="hasDrinkEntries" icon={faCocktail} /> : null}
+        </td>
+        <td style={centerStyle}>
+          {this.props.data.hasMedicationEntries ? <FontAwesomeIcon key="hasMedicationEntries" icon={faCapsules} /> : null}
+        </td>
+        <td style={centerStyle}>
+          {this.props.data.hasPhotoEntries ? <FontAwesomeIcon key="hasPhotoEntries" icon={faCamera} /> : null}
+        </td>
+      </tr>
     );
   }
 }
 
 class CollectionList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date().toISOString().substr(0, 10),
+    }
+  }
   static propTypes = {
     collections: PropTypes.array.isRequired,
   }
@@ -34,23 +54,59 @@ class CollectionList extends Component {
     this.props.getCollections();
   }
 
+  handleChange(event) {
+    this.setState({
+      date: event.target.value,
+    })
+  }
+
+  handleClick(e) {
+    this.props.createCollection(this.state.date);
+    e.preventDefault();
+  }
+
   render() {
+    let centerStyle = { textAlign: 'center' };
     return (
       <div className="container">
-      <h1>There are {this.props.collections.length} entries in your diary!</h1>
-      <div>
-        <DatePicker selected={this.props.date} onChange={date => this.props.selectDate(date)}/>
-        <button
-          className="btn btn-sm btn-primary ml-2"
-          onClick={() => this.props.createCollection(this.props.date)} >
-          Create new
-        </button>
-      </div>
-      <ul>
-      {
-        this.props.collections.map((col) => <CollectionListItem key={col.id} data={col}/> )
-      }
-      </ul>
+        <Accordion className="mt-2">
+          <Card>
+            <Accordion.Toggle eventKey="0" as={Card.Header} className="text-right" variant="link">
+              Add more...
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>
+                <Form>
+                  <Form.Group>
+                    <Form.Label>Date</Form.Label>
+                    <Form.Control type="date" value={this.state.date} onChange={date => this.handleChange(date)} />
+                  </Form.Group>
+                  <Button
+                    size="sm"
+                    variant="primary"
+                    type="submit"
+                    onClick={(e) => this.handleClick(e)} >
+                    Create new
+                  </Button>
+                </Form>
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
+        <Table hover striped bordered size="sm" className="mt-2">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th style={centerStyle}><FontAwesomeIcon key="hasTextEntries" icon={faComment} /></th>
+              <th style={centerStyle}><FontAwesomeIcon key="hasDrinkEntries" icon={faCocktail} /></th>
+              <th style={centerStyle}><FontAwesomeIcon key="hasMedicationEntries" icon={faCapsules} /></th>
+              <th style={centerStyle}><FontAwesomeIcon key="hasMedicationEntries" icon={faCamera} /></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.collections.map((col) => <CollectionListRow key={col.id} data={col} />)}
+          </tbody>
+        </Table>
       </div>
     );
   }
@@ -58,7 +114,6 @@ class CollectionList extends Component {
 
 const mapStateToProps = state => ({
   collections: state.collections.collections,
-  date: state.collections.date,
 });
 
-export default connect(mapStateToProps, { getCollections, selectDate, createCollection })(CollectionList);
+export default connect(mapStateToProps, { getCollections, createCollection })(CollectionList);
