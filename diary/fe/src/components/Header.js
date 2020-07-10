9dea5fad16462a09ebb94fbcff2384053d4f5b6c
Pierre <pierre.git@posteo.de>
2020-07-10 18:14:22 +0200

    Header: uses react-bootstrap for the navbar

--- diary/fe/src/components/Header.js
+++ diary/fe/src/components/Header.js
@@ -1,8 +1,11 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../actions/auth';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
 
 class Header extends Component {
 static propTypes = {
@@ -13,52 +16,23 @@ static propTypes = {
       const { isAuthenticated, user } = this.props.auth;
 
       const authLinks = (
-        <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-          <span className="navbar-text mr-3">
-            <strong>{user ? `Welcome ${user.first_name}` : ''}</strong>
-          </span>
-          <li className="nav-item">
-            <button onClick={this.props.logout} className="nav-link btn btn-info btn-sm text-light">
-              Logout
-            </button>
-          </li>
-        </ul>
+        <Fragment>
+        <strong className="mr-2">{user ? `Welcome, ${user.first_name}!` : ''}</strong>
+        <Nav.Link as={Button} onClick={this.props.logout}>Logout</Nav.Link>
+        </Fragment>
       );
-  const guestLinks = (
-        <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-          <li className="nav-item">
-            <Link to="/login" className="nav-link">
-              Login
-            </Link>
-          </li>
-        </ul>
-      );
+      const guestLinks = <Nav.Link as={Link} to="/login">Login</Nav.Link>;
+
         return (
           <div className="container">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">
-                  Diary
-                </a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarNavAltMarkup"
-                    aria-controls="navbarNavAltMarkup"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <a className="nav-item nav-link active" href="#">
-                            All Entries <span className="sr-only">(current)</span>
-                        </a>
-                    </div>
-          {isAuthenticated ? authLinks : guestLinks}
-                </div>
-            </nav>
+          <Navbar expand="lg" bg="light">
+            <Navbar.Brand href="/">Diary</Navbar.Brand>
+
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-end">
+                    {isAuthenticated ? authLinks : guestLinks}
+            </Navbar.Collapse>
+          </Navbar>
           </div>
         );
     }
