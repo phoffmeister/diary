03485a36166250ae6f786d3284ffaf116e6366d1
Pierre <pierre.git@posteo.de>
2020-07-12 00:45:46 +0200

    config: moves style loader to dev dep

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ]
-    }
-}
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
