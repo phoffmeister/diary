a50bd3e6d1654ed9e0ddc0565a3f6e4561608124
Pierre <pierre.git@posteo.de>
2020-07-15 16:01:08 +0200

    EntryAccrodion: adds an indicator

--- diary/fe/src/components/collections/detail/EntryAccordion.js
+++ diary/fe/src/components/collections/detail/EntryAccordion.js
@@ -1,17 +1,41 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext } from "react";
 import Card from "react-bootstrap/Card";
 import Accordion from "react-bootstrap/Accordion";
+import AccordionContext from "react-bootstrap/AccordionContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleRight, faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { useAccordionToggle } from "react-bootstrap/AccordionToggle";
+
+function ContextAwareToggle({ children, eventKey, callback, as }) {
+  const currentEventKey = useContext(AccordionContext);
+
+  const decoratedOnClick = useAccordionToggle(
+    eventKey,
+    () => callback && callback(eventKey)
+  );
+
+  const isCurrentEventKey = currentEventKey === eventKey;
+  const AsElement = as;
+
+  return (
+    <AsElement onClick={decoratedOnClick}>
+      {isCurrentEventKey ? (
+        <FontAwesomeIcon icon={faAngleDown} />
+      ) : (
+        <FontAwesomeIcon icon={faAngleRight} />
+      )}{" "}
+      {children}
+    </AsElement>
+  );
+}
 
 export default function EntryAccordion(props) {
   const EntryFrom = props.form;
   return (
     <Card>
-      <Accordion.Toggle
-        eventKey={props.accordionID}
-        as={Card.Header}
-        variant="link">
+      <ContextAwareToggle as={Card.Header} eventKey={props.accordionID}>
         {props.title} Entries
-      </Accordion.Toggle>
+      </ContextAwareToggle>
       <Accordion.Collapse eventKey={props.accordionID}>
         <Fragment>
           <Card.Body>{props.entries.map(props.mapper)}</Card.Body>
