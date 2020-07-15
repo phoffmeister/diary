9488a10f9761765defaf204945dc0eb17eaa5e9c
Pierre <pierre.git@posteo.de>
2020-07-15 15:12:58 +0200

    PhotoEntryForm: photo input now clears upon successful submission

--- diary/fe/src/components/collections/detail/forms/CreatePhotoEntryForm.js
+++ diary/fe/src/components/collections/detail/forms/CreatePhotoEntryForm.js
@@ -4,22 +4,43 @@ import Button from "react-bootstrap/Button";
 import { createPhoto } from "../../../../actions/photo";
 import { connect } from "react-redux";
 
+const noop = () => {};
+
+const FileInput = ({ value, onChange = noop, ...rest }) => (
+  <div>
+    {Boolean(value.length) && <div>Selected file: {value}</div>}
+    <label>
+      Click to select a photo...
+      <input
+        {...rest}
+        style={{ display: "none" }}
+        type="file"
+        onChange={(e) => {
+          onChange(e);
+        }}
+      />
+    </label>
+  </div>
+);
+
 class CreatePhotoEntryForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
       photo: null,
       caption: "",
+      photoFilename: "",
     };
   }
 
   handleClick(event) {
+    event.preventDefault();
+    if (this.state.photoFilename === "") return;
     const formData = new FormData();
     formData.append("photo", this.state.photo);
     formData.append("collection", this.props.dayID);
     formData.append("caption", this.state.caption);
     this.props.createPhoto(formData, () => this.clearForm());
-    event.preventDefault();
   }
 
   handleChange(event) {
@@ -30,6 +51,7 @@ class CreatePhotoEntryForm extends Component {
     } else if (event.target.name == "photo") {
       this.setState({
         photo: event.target.files[0],
+        photoFilename: event.target.files[0].name,
       });
     }
   }
@@ -37,6 +59,8 @@ class CreatePhotoEntryForm extends Component {
   clearForm() {
     this.setState({
       caption: "",
+      photoFilename: "",
+      photo: null,
     });
   }
 
@@ -44,9 +68,10 @@ class CreatePhotoEntryForm extends Component {
     return (
       <Form onSubmit={(e) => this.handleClick(e)}>
         <Form.Group>
-          <Form.File
+          <FileInput
             name="photo"
             label="Photo"
+            value={this.state.photoFilename}
             onChange={(e) => this.handleChange(e)}
           />
         </Form.Group>
